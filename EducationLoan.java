3dd18616e9c11cfe377242e16a3a487c589b211e
Yuvraj666 <56020931+Yuvraj666@users.noreply.github.com>
2019-10-04 10:02:44 +0530

    Add files via upload

---
+++ EducationLoan.java
@@ -0,0 +1,21 @@
+package com.cg.ibs.loanmgmt.bean;
+
+import java.util.Scanner;
+
+public class EducationLoan extends LoanDetails {
+
+	public final float interestRate = 11.35f;
+
+	@Override
+	public double getLoanLimit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLoanLimit() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ VehicleLoan.java
@@ -0,0 +1,17 @@
+package com.cg.ibs.loanmgmt.bean;
+
+public class VehicleLoan extends LoanDetails {
+	public final float interestRate = 9.25f;
+	@Override
+	public double getLoanLimit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLoanLimit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
